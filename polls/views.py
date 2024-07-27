bdece9463bbee1526c7e06a8f705d020f4cb38b6
Clebson Moura <clebson.sh@gmail.com>
2024-07-27 20:06:53 -0300

    update detail method to get question from database and show in the view

--- polls/views.py
+++ polls/views.py
@@ -9,8 +9,13 @@ def index(request):
     return render(request, "polls/index.html", {
         "latest_question_list": latest_question_list,
     })
+
 def detial(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
